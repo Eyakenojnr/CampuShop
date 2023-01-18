4c9238629987186f350a6d34e660027a716edb23
Joshua Alonge <alongejoshua@gmail.com>
2023-01-18 23:14:48 +0100

    creating seller model

--- models.py
+++ models.py
@@ -14,7 +14,14 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
+class Seller():
+    # This is the dataset structure for the sellers
+    id = db.Column(db.Integer, Primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
 
 class Product():
 
