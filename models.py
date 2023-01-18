91e1d851a7abebdabc1ae015b0939a82fb7ac10d
Joshua Alonge <alongejoshua@gmail.com>
2023-01-18 23:14:19 +0100

    creating the User/buyer model for the db

---
+++ models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    # This is the dataset structure for the buyer / users
+    id = db.Column(db.Integer, Primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+class Product():
+
+
+
+class Service()
+
